8b08242f62d61d622d77663ff3df3bd34577e948
pt10598 <130521177+pt10598@users.noreply.github.com>
2023-07-28 16:40:43 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+from flask import Flask, request, render_template, redirect, url_for
 from flask import Flask
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def formPage():
+    return render_template('page.html')
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run(host="0.0.0.0",port=5000,debug=True)
