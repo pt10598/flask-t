e2724024766f66ea86629bab99dd0938496ec721
pt10598 <130521177+pt10598@users.noreply.github.com>
2023-07-28 16:35:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template
-
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-  return render_template('index.html')
+@app.route("/")
+def hello():
+    return "Hello World!"
 
-if __name__ == '__main__':
-  app.run(port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5000,debug=True)
