3f61e2997142bf4778634716a5a7f289c17efef2
pt10598 <130521177+pt10598@users.noreply.github.com>
2023-07-28 17:30:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,16 +11,31 @@ def submit():
         user = request.form['user']
         tte = request.form['user2']
         tte2 = request.form['user3']
-
-        return redirect(url_for('success', name=user,serc6=tte,serc9=tte2))
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        driver = webdriver.Chrome(options=chrome_options)
+        driver.get("https:pay.taipei/v2/CheckBill/Index/2")
+        time.sleep(0.5)
+        serc2 = driver.find_element("name", "parameter2")
+        serc2.send_keys(tte2)
+        time.sleep(0.5)
+        serc3 = driver.find_element("xpath", '/html/body/article/main/form/div[3]/button')
+        serc3.click()
+        time.sleep(0.5)
+        serc4 = driver.find_element("xpath", '/html/body/article/main/form/section/div[4]/button')
+        serc4.click()
+        time.sleep(0.5)
+        serc12 = driver.find_element("xpath", '/html/body/article/main/form[1]/div[1]/div/span[2]')
+        return redirect(url_for('success', name=user,serc6=tte,serc9=serc12.text))
 
 @app.route('/success/<name>/<serc6>/<serc9>/')
 def success(name,serc6,serc9):
-    return f"<h2>{name}</h2>"\
+    sst=int(serc9)
+    return "<title>查詢網</title>"\
+            f"<h2>{name}</h2>"\
            f"<h2>{serc6}</h2>"\
-           f"<h2>{serc9}</h2>"\
+           f"<h2>{sst}元</h2>"\
            "<h2><input type='button' value='返回首頁' onclick='history.back()' style='width:90px;height:40px;'/></h2>"\
-
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=5000,debug=True)
