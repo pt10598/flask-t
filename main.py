8038feb501e9ab339f74640e28007518d4c2e683
pt10598 <130521177+pt10598@users.noreply.github.com>
2023-07-28 17:30:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, request, render_template, redirect, url_for
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 app = Flask(__name__)
 
 @app.route('/')
